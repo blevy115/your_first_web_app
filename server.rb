fb4c72da61c55d8b8b0a74685cdd1b95918cec1a
Brandon Levy <brandon.levy@mail.mcgill.ca>
2017-11-15 18:04:49 -0500

    added favourites and reditrects

--- server.rb
+++ server.rb
@@ -10,5 +10,21 @@ get '/portfolio' do
 end
 
 get '/about_me' do
+  @skills = ['git', 'HTML', 'CSS', 'Ruby']
+  @interests = ['sports', 'books', 'video games', 'tv shows']
   erb :about_me
+end
+
+
+get '/favourites' do
+  @fav_links = ['google.com', 'reddit.com', 'youtube.com', 'facebook.com', 'netflix.com']
+  erb :favourites
+end
+
+get '/' do
+  redirect to ('/home')
+end
+
+get '/gallery' do
+  redirect to ('/portfolio')
 end
